fed867af11b3bb5205a51ded1544f22ac76856b3
Andrew T <andrewtgit@gmail.com>
2023-02-01 19:45:11 +1100

    Added error messages to submit function.

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ function submitForm() {
     const form = document.querySelector('form')
     const pass1 = document.getElementById('password')
     const pass2 = document.getElementById('password-confirm')
+    const label1 = document.getElementById('error-label')
 
-    if (pass1.value == pass2.value && (pass1.value || pass2.value) != '') {
-        form.submit()
-        console.log("Submitted")
+    if (pass1.value == '' || pass2.value == '') {
+        label1.setAttribute('class', 'error-empty')
+    } else if (pass1.value == pass2.value) {
+            form.submit()
     } else {
         pass1.setAttribute('class', 'error')
         pass2.setAttribute('class', 'error')
-        console.log("Error")
+        label1.setAttribute('class', 'error')
     }
 }
 
